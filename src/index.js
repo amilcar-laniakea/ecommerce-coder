6813058cf28c61611881e3d8a9e178b00a74c2e4
Amilcar Barahona <amilcar.laniakea@gmail.com>
2021-06-26 11:16:34 -0400

    borrado reportWebVItals

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import reportWebVitals from './reportWebVitals'
 
 import Router from './router'
 
 import './style.css'
 
 ReactDOM.render(<Router />, document.getElementById('root'))
-
-reportWebVitals()
